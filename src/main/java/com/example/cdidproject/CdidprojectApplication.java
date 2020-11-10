813c3f7e2fdcf6cab6b544a7520a1479cade181a
Kristopher Lea <kristopherlea187@gmail.com>
2020-11-10 08:58:20 -0600

    first commit

---
+++ src/main/java/com/example/cdidproject/CdidprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cdidproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdidprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdidprojectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/cdidproject/MathController.java
@@ -0,0 +1,16 @@
+package com.example.cdidproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+
+    @GetMapping("/add/{x}/{y}")
+    public int addIntegers(@PathVariable int x,@PathVariable int y){
+        return x + y;
+    }
+
+}
